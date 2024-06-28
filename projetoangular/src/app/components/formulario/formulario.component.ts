8772d8b8815e1105c01e90c52d8a8900a47ca3c1
Leo Luz <8610316+leopluz@users.noreply.github.com>
2024-06-27 21:43:32 -0300

    Formulário

--- projetoangular/src/app/components/formulario/formulario.component.ts
+++ projetoangular/src/app/components/formulario/formulario.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  nome = "";
+
+  mostrarNome() {
+    alert(this.nome);
+  }
+
+  formularioPerfil = new FormGroup(
+    {
+      nomeCompleto: new FormControl('', Validators.required),
+      email: new FormControl('', [ Validators.required, Validators.email ])
+    }
+  );
+  enviarFormulario() {
+    alert( 
+      "Nome Completo: " 
+      + this.formularioPerfil.value.nomeCompleto 
+      + "\nE-mail: "
+      + this.formularioPerfil.value.email
+    );
+  }
 
 }
